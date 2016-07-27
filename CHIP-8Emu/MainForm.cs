760f13db8df1f5f2ea2330b2ade0816bab9929ba
gilmoa <agilmozzi@gmail.com>
2016-07-28 00:51:39 +0200

    CPU and Timers Clocks. Handle sound duration

--- CHIP-8Emu/MainForm.cs
+++ CHIP-8Emu/MainForm.cs
@@ -5,6 +5,7 @@ using System.Drawing.Imaging;
 using System.Drawing.Drawing2D;
 using System.Threading.Tasks;
 using System.Threading;
+using System.Collections.Generic;
 
 namespace CHIP_8Emu
 {
@@ -13,7 +14,7 @@ namespace CHIP_8Emu
         private Chip8 Chip8;
         private Bitmap screen;
         private TimeSpan clock = TimeSpan.FromTicks(TimeSpan.TicksPerSecond / 400);
-        private TimeSpan screenFrequency = TimeSpan.FromTicks(TimeSpan.TicksPerSecond / 60);
+        private TimeSpan screenClock = TimeSpan.FromTicks(TimeSpan.TicksPerSecond / 60);
 
         public MainForm()
         {
@@ -21,15 +22,64 @@ namespace CHIP_8Emu
 
             screen = new Bitmap(64, 32, PixelFormat.Format32bppArgb);
             screenPB.Image = screen;
+
+            KeyDown += MapKeyDown;
+            KeyUp += MapKeyUp;
         }
 
         protected override void OnLoad(EventArgs e)
         {
             Chip8 = new Chip8(DrawScreen, Beep);
 
-            Chip8.LoadROM("rom/PONG");
+            Chip8.LoadROM("rom/INVADERS");
 
-            Task.Run(MainLoop);
+            Task.Run(CPULoop);
+            Task.Run(TimersLoop);
+        }
+
+        Task CPULoop()
+        {
+            while (true)
+            {
+                Chip8.Cycle();
+                Thread.Sleep(clock);
+            }
+        }
+
+        Task TimersLoop()
+        {
+            while (true)
+            {
+                Chip8.TimerCycle();
+                Thread.Sleep(screenClock);
+            }
+        }
+
+        private Dictionary<Keys, byte> KeyMap = new Dictionary<Keys, byte>()
+        {
+            { Keys.D1, 0x1 }, { Keys.D2, 0x2 },
+            { Keys.D3, 0x3 }, { Keys.D4, 0xc },
+
+            { Keys.Q, 0x4 }, { Keys.W, 0x5 },
+            { Keys.E, 0x6 }, { Keys.R, 0xd },
+
+            { Keys.A, 0x7 }, { Keys.S, 0x8 },
+            { Keys.D, 0x9 }, { Keys.F, 0xe },
+
+            { Keys.Z, 0xa }, { Keys.X, 0x0 },
+            { Keys.C, 0xb }, { Keys.V, 0xf }
+        };
+
+        private void MapKeyDown(object sender, KeyEventArgs e)
+        {
+            if (KeyMap.ContainsKey(e.KeyCode))
+                Chip8.KeyDown(KeyMap[e.KeyCode]);
+        }
+
+        private void MapKeyUp(object sender, KeyEventArgs e)
+        {
+            if (KeyMap.ContainsKey(e.KeyCode))
+                Chip8.KeyUp(KeyMap[e.KeyCode]);
         }
 
         private void DrawScreen(bool[,] gfx)
@@ -40,22 +90,12 @@ namespace CHIP_8Emu
                     screen.SetPixel(x, y, gfx[x, y] ? Color.White : Color.Black);
                 }
 
-
             screenPB.Image = ResizeBitmap(screen, screenPB.Width, screenPB.Height);
         }
 
-        private void Beep()
+        private void Beep(int ms)
         {
-            Console.Beep(459, 96);
-        }
-
-        Task MainLoop()
-        {
-            while (true)
-            {
-                Chip8.Cycle();
-                Thread.Sleep(clock);
-            }
+            Console.Beep(459, ms);
         }
 
         private Bitmap ResizeBitmap(Bitmap original, int width, int height, InterpolationMode interpolation = InterpolationMode.NearestNeighbor)
